6441479416b7678a96a1dcd0653d6a08710606b2
Sachin Varghese <sachinv2223@gmail.com>
2022-11-10 23:42:30 +0530

    not hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   ROOT_URL: 'http://localhost:4000'
-  // ROOT_URL: 'https://splanner-apiserver.herokuapp.com'
+  // ROOT_URL: 'https://splanner-api.onrender.com'
 };
 
 /*
